65dd56210380b6b7bbdf7c7c86340640ca390487
Gabriela Molina Ciocci <gabi.ciocci@hotmail.com>
2024-05-23 15:43:20 -0300

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,205 @@
+export const categories = [
+  {
+    id: 1,
+    title: "Todos"
+  },
+  {
+    id: 2,
+    title: "Top Músicas"
+  },
+  {
+    id: 3,
+    title: "Artistas"
+  },
+  {
+    id: 4,
+    title: "Albuns"
+  },
+  {
+    id: 5,
+    title: "Lançamentos"
+  }
+]
+
+export const instruments = [
+  {
+    id: 1,
+    title: "Todos",
+  },
+  {
+    id: 2,
+    title: "VS",
+  },
+  {
+    id: 3,
+    title: "Click",
+  },
+  {
+    id: 4,
+    title: "Bateria",
+  },
+  {
+    id: 5,
+    title: "Piano",
+  },
+  {
+    id: 6,
+    title: "Guitarra",
+  },
+  {
+    id: 7,
+    title: "Vozes",
+  },
+  {
+    id: 8,
+    title: "Baixo",
+  },
+]
+
+export const musicItems = [
+  {
+    name: "Quem é Como Nosso Deus?",
+    singer: "Nivea Soares",
+    image: require('../assets/musica1.png'),
+    page: 'Musica1',
+    id: 1,
+  },
+  {
+    name: "Levanto um Aleluia",
+    singer: "Julia Vitoria",
+    image: require('../assets/musica6.png'),
+    page: 'Musica6',
+    id: 2,
+  },
+  {
+    name: "Hosana",
+    singer: "Nivea Soares",
+    image: require('../assets/musica2.png'),
+    page: 'Musica2',
+    id: 3,
+  },
+    {
+    name: "Palavras",
+    singer: "Julia Vitoria",
+    image: require('../assets/musica7.png'),
+    page: 'Musica7',
+    id: 4,
+  },
+  {
+    name: "Grande é o Senhor",
+    singer: "Nivea Soares",
+    image: require('../assets/musica3.png'),
+    page: 'Musica3',
+    id: 5,
+  },
+  {
+    name: "Bondade de Deus",
+    singer: "Isaias Saad",
+    image: require('../assets/musica4.png'),
+    page: 'Musica4',
+    id: 6,
+  },
+  {
+    name: "Ruja o Leão + Que se Abram os Céus",
+    singer: "Isaias Saad, Nivea Soares",
+    image: require('../assets/musica5.png'),
+    page: 'Musica5',
+    id: 7,
+  },
+  {
+    name: "Além do Rio Azul",
+    singer: "Julia Vitoria",
+    image: require('../assets/musica8.png'),
+    page: 'Musica8',
+    id: 8,
+  },
+]
+
+export const singerItems = [
+  {
+    name: "Nivea Soares",
+    photo: require('../assets/nivea.png'),
+    page: 'Perfil1',
+    id: 1,
+  },
+  {
+    name: "Isaias Saad",
+    photo: require('../assets/isaias.png'),
+    page: 'Perfil2',
+    id: 2,
+  },
+  {
+    name: "Julliany Souza",
+    photo: require('../assets/ju.png'),
+    page: 'Perfil3',
+    id: 3,
+  },
+  {
+    name: "Gabriela Rocha",
+    photo: require('../assets/gabi.png'),
+    page: 'Perfil4',
+    id: 4,
+  },
+  {
+    name: "Julia Vitória",
+    photo: require('../assets/juvi.png'),
+    page: 'Perfil5',
+    id: 5,
+  },
+  {
+    name: "Elevation Worship",
+    photo: require('../assets/elevation.png'),
+    page: 'Perfil6',
+    id: 6,
+  },
+  {
+    name: "Fhop Music",
+    photo: require('../assets/fhop.png'),
+    page: 'Perfil7',
+    id: 7,
+  },
+  {
+    name: "Gabriel Guedes",
+    photo: require('../assets/gabguedes.png'),
+    page: 'Perfil8',
+    id: 8,
+  },
+]
+
+export const albumItems = [
+  {
+    name: "CAN YOU IMAGINE?",
+    singer: "Elevation Worship",
+    photo: require('../assets/elevation_album.png'),
+    page: 'Album1',
+    id: 1,
+  },
+  {
+    name: "A Boa Parte",
+    singer: "Fhop Music",
+    photo: require('../assets/album_fhop.png'),
+    page: 'Album2',
+    id: 2,
+  },
+  {
+    name: "A Presença",
+    singer: "Gabriela Rocha",
+    photo: require('../assets/album_gabi.png'),
+    page: 'Album3',
+    id: 3,
+  },
+  {
+    name: "Quem é Como Nosso Deus?",
+    singer: "Nivea Soares",
+    photo: require('../assets/musica1.png'),
+    page: 'Album4',
+    id: 4,
+  },
+  {
+    name: "Glória e Honra",
+    singer: "Nivea Soares",
+    photo: require('../assets/ni_album.png'),
+    page: 'Album5',
+    id: 5,
+  }
+]
